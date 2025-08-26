0e407ff04905698cc0b70011e6bc5f98a651fa13
sarthakaggarwal19032001 <1000012262@dit.edu.in>
2025-08-26 18:09:40 +0530

    06:09

--- Backend/slog/main/serializers.py
+++ Backend/slog/main/serializers.py
@@ -15,6 +15,12 @@ class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = '__all__'
+    
+    def get_image(self, obj):
+        request = self.context.get("request")
+        if obj.image and hasattr(obj.image, "url"):
+            return request.build_absolute_uri(obj.image.url)
+        return None
 
 
 class GallerySerializer(serializers.ModelSerializer):
